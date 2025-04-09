0f250816332d0ae7b6232059b2507bfe6c98443d
GoShIYO <k024g1025@g.neec.ac.jp>
2025-04-09 13:57:50 +0900

    KamataEngineを入れた

---
+++ DirectXGame/main.cpp
@@ -0,0 +1,39 @@
+#include <Windows.h>
+#include"KamataEngine.h"
+#include"GameScene.h"
+#include<memory>
+
+using namespace KamataEngine;
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+
+	KamataEngine::Initialize(L"LE2A_20_リ_ショウコウ_AL3");
+
+	// DirectXCommonのインスタンスの取得
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	// GameSceneのインスタンスの作成
+	std::unique_ptr<GameScene> gameScene = std::make_unique<GameScene>();
+    gameScene->Initialize();
+
+    while (true) {
+		if (KamataEngine::Update()) {
+			break;
+		}
+		
+		//ゲームシーンの更新
+		gameScene->Update();
+		
+		//描画開始
+		dxCommon->PreDraw();
+		
+		//ゲームシーンの描画
+		gameScene->Draw();
+		
+		//描画終了
+		dxCommon->PostDraw();
+    	
+	}
+
+	return 0;
+}
